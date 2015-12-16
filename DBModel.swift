f87035432df997e87b72b187bbe324ed32327c8a
David Higgins <david@gryphonandrook.com>
2015-12-16 11:42:00 -0700

    Validate before save

--- DBModel.swift
+++ DBModel.swift
@@ -168,10 +168,12 @@ class DBModel: NSManagedObject {
     func save() {
         if currentContext.hasChanges {
             do {
+                try self.validateForInsert()
                 try currentContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                self.delete()
                 let nserror = error as NSError
                 NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
                 abort()
